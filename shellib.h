c7c23d96a04726e568a17ee80987aa086df62298
Degreatjoe <jgreat770@gmail.com>
2024-02-25 14:04:42 +0100

    update

--- shellib.h
+++ shellib.h
@@ -1,5 +1,14 @@
 #ifdef SHELLIB_H
 #define SHELLIB_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+
+
 
 #endif
